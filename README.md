0414ec3c6ab982106cbe30b4cfc89ff08b3cb507
Morales Palomar <adrian.morales@cunef.edu>
2023-10-14 20:23:32 +0200

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PortfolioApp
+# AesyPortfolioweb
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.6.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'portfolio-app'`, () => {
+  it(`should have as title 'aesy_portfolioweb'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('portfolio-app');
+    expect(app.title).toEqual('aesy_portfolioweb');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('portfolio-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('aesy_portfolioweb app is running!');
   });
 });
